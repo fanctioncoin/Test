5061772946f2a0b5d4307b27da52f9a3b4f65b89
Vyacheslav Staravoyt <v.staravoyt@reply.com>
2022-03-14 10:59:50 +0300

    Initial commit

---
+++ src/com/test/app/Runner.java
@@ -0,0 +1,25 @@
+package com.test.app;
+
+public class Runner {
+    public static void main(String[] args) {
+         String check ="test2";
+        final String siteId = null;
+        Boolean i =true;
+        System.out.println(Boolean.valueOf(i));
+
+
+        if (!siteId.isEmpty())
+        {
+            System.out.printf("Test");;
+        }
+        if (!check.isEmpty())
+        {
+            System.out.printf("test2");
+
+            return;
+        }
+        else {
+            System.out.printf("test3333333");
+        }
+    }
+}
